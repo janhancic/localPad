d37134ae6425688ebdbf0f1951735922f4aa1b86
Jan Hančič <jan.hancic@gmail.com>
2013-01-29 22:05:16 +0100

    basic styling

--- localpad.js
+++ localpad.js
@@ -4,10 +4,26 @@
 		return;
 	}
 
-	var html = '<!DOCTYPE html><html><head><title>untitled pad</title></head>'
-		+ '<body><aside>aside</aside>'
-		+ '<section></section></body></html>'
+	var html = '<!DOCTYPE html><html><head>'
+		+ '<title>untitled pad</title>'
+		+ '<style>'
+		+ 'html, body { height: 100%; position: relative; margin: 0; padding: 0 }'
+		+ 'aside { position: absolute; left: 0; top: 0; width: 150px; height: 100%; border-right: 1px solid #ccc; }'
+		+ 'section { margin-left: 160px; height: 100%; }'
+		+ '#title { width: 99%; }'
+		+ '#content { width: 99%; height: 91%; }'
+		+ '</style>'
+		+ '</head><body>'
+		+ '<aside id="pads">test</aside>'
+		+ '<section><input type="text" id="title" /><textarea id="content"></textarea></section>'
+		+ '</body></html>'
 	;
 
-	document.open ( 'text/html', 'replace' ).write ( html ).close ();
+	var newDocument = document.open ( 'text/html', 'replace' );
+	newDocument.write ( html );
+	newDocument.close ();
+
+	// var padsEl = document.getElementById ( 'pads' );
+	// var titleEl = document.getElementById ( 'title' );
+	// var contentEl = document.getElementById ( 'content' );
 } ( window, document ) );
